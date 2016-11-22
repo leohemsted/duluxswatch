49c52f9187bf90a82341a893a3ef50ee6cf8eef7
Leo Hemsted <leohemsted@gmail.com>
2016-11-22 00:01:37 +0000

    get colours from dulux api

--- duluxswatch/app.py
+++ duluxswatch/app.py
@@ -1,41 +1,41 @@
 from string import Template
 
 import tinydb
+import requests
 from flask import Flask
 
 app = Flask(__name__)
 db = tinydb.TinyDB('./db.json')
 
 HTML = """<!DOCTYPE html>
-    <html>
-     <head>
-      <title>${name} - Dulux Swatch</title>
-      <link rel="StyleSheet" href="static/main.css" type="text/css">
-     </head>
-     <body style="background-color: rgb(${r}, ${g}, ${b})">${name}</div>
-     </body>
-    </html>
+<html>
+  <head>
+    <title>Dulux Swatch</title>
+    <link rel="StyleSheet" href="static/main.css" type="text/css">
+  </head>
+  <body style="background-color: #${rgb}">
+    ${name} (id: ${id})
+  </body>
+</html>
 """
 
-
-@app.route('/<colour_id>')
-def get_colour(colour_id):
+@app.route('/id/<colour_id>')
+def get_colour_by_id(colour_id):
     obj = db.get(tinydb.Query().id == colour_id)
 
     if not obj:
         obj = fetch_colour(colour_id)
+        from pprint import pprint
+        pprint(obj)
         db.insert(obj)
 
-    return Template(HTML).substitute(data)
+    return Template(HTML).substitute(obj)
 
 def fetch_colour(colour_id):
     """
     Fetch RGB/name info from dulux
     """
-    return {
-        'id': colour_id,
-        'name': 'mauve',
-        'r': 100,
-        'g': 20,
-        'b': 50
-    }
+    response = requests.get('https://www.dulux.co.uk/en/api/color/{0}'.format(colour_id))
+    response.raise_for_status()
+
+    return response.json()
